4247c16d11ffd766da25335f551976af30a11168
yurain <ryanu9@163.com>
2024-12-25 18:03:25 +0800

    修复\wms\basic\savenewinout.php处 type name cost id 4种参数注入

--- src/basic/savenewinout.php
+++ src/basic/savenewinout.php
@@ -1,9 +1,29 @@
 <?php
 include("../conn/conn.php");
-$type=$_POST[type];
-$name=$_POST[name];
-$cost=$_POST[cost];
-$id=$_GET[id];
+$type = $_POST['type'];
+$name = $_POST['name'];
+$cost = $_POST['cost'];
+$id = $_GET['id'];
+
+// 限制$type只能是字母或数字
+if (!preg_match('/^[a-zA-Z0-9]+$/', $type)) {
+    die("<script>alert('非法输入！');history.back();</script>");
+}
+
+// 对$name做类似限制
+if (!preg_match('/^[a-zA-Z0-9\s]+$/', $name)) {
+    die("<script>alert('非法输入！');history.back();</script>");
+}
+
+// 确保$cost是数字
+if (!is_numeric($cost)) {
+    die("<script>alert('非法金额输入！');history.back();</script>");
+}
+
+// 确保$id是数字
+if (!empty($id) && !is_numeric($id)) {
+    die("<script>alert('非法ID输入！');history.back();</script>");
+}
 if($id!="")
 {
        $sql=mysql_query("select * from tb_inout where id='".$id."'",$conn);
